c8946d3843097e52b9ef8c1d46a1f3844491ddce
Jacob Reed <JacobReed@Jacobs-MacBook-Pro.local>
2015-12-20 10:29:46 -0600

    Made changes to fix the issue with admin posting

--- Django_App/src/joins/views.py
+++ Django_App/src/joins/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .forms import EmailForm
 
 # Create your views here.
 
 #Moves the home page view to the joins app
 def home(req):
-	context = {} #Imports the python dictionary
+	form = EmailForm() #Creates a form where we can get the email from
+	context = {"form": form} 
 	template = "home.html" #Picks the right template for the page
 	return render(req, template, context) #Renders everything for the page 
