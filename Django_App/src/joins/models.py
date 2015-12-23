e76dab1c8e75a8cf5d5533397d8fd74d8d2beafd
Jacob Reed <JacobReed@Jacobs-MacBook-Pro.local>
2015-12-22 19:44:14 -0600

    Implementing model to collect user's IP

--- Django_App/src/joins/models.py
+++ Django_App/src/joins/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Join(models.Model):
 	email = models.EmailField() #Adds a feild for the user's email
+	ip_address = models.CharField(max_length=120, default='ABC') #Sets the variable to the user's current IP
 	timestamp = models.DateTimeField(auto_now_add = True, auto_now = False) #Adds the date and time to the page
 	updates = models.DateTimeField(auto_now_add = False, auto_now = True)
 	name = models.TextField()
