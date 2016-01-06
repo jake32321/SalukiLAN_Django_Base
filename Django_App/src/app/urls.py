d92760ec4caba7ea232519b27d5f7549b58fb4cc
jake32321 <jake32321@yahoo.com>
2016-01-05 18:33:05 -0600

    Added possible URLs for the actual site.

--- Django_App/src/app/urls.py
+++ Django_App/src/app/urls.py
@@ -13,4 +13,16 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'joins.views.home', name='home'), #Home URL
     url(r'^jakeisdaman/', include(admin.site.urls)), #URL for the admin panel
+
+    # Possible URLs that will be needed.
+
+    # The 'namehere' will be the part with the file name located in the views dir.
+
+    # url(r'^about/', 'joins.views.#namehere', name='about'),
+    # url(r'^event-rules/', 'joins.views.#namehere', name='event-rules'),
+    # url(r'^tournaments/', 'joins.views.#namehere', name='tournaments'),
+    # url(r'^gallery/', 'joins.views.#namehere', name='gallery'),
+    # url(r'^sponsors/', 'joins.views.#namehere', name='sponsors'),
+    # url(r'^reserve-a-seat/', 'joins.views.#namehere', name='reserve-a-seat'),
+    # url(r'^my-account/', 'joins.views.#namehere', name='my-account'),
 )
