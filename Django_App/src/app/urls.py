0252a7e1b41e80cbcdcc76567697bedffb8b3604
Jacob Reed <JacobReed@Jacobs-MacBook-Pro.local>
2016-01-05 18:19:42 -0600

    Started working on the URLs for the actual site

--- Django_App/src/app/urls.py
+++ Django_App/src/app/urls.py
@@ -6,7 +6,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'joins.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
 )
