4ad45d12e021862c4b50ce45de3c7884f9924873
Jacob Reed <JacobReed@Jacobs-MacBook-Pro.local>
2016-01-05 18:23:43 -0600

    Added information regarding URLs

--- Django_App/src/app/urls.py
+++ Django_App/src/app/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+#URLs are stored as a tuple.
+
+# Working on finding all of the necessary URLs we
+# will need in the end.  Make mention if you see
+# any that need to be added.
+
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'joins.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'joins.views.home', name='home'), #Home URL
+    url(r'^jakeisdaman/', include(admin.site.urls)), #URL for the admin panel
 )
